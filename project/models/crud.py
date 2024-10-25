958a197b4a1ca26e54f067f870d95e2d67f59113
v-statura <valerarl19mgtu@gmail.com>
2024-10-25 23:13:29 +0300

    чуть чуть переписал круд вопросы по поводу валидации

--- project/models/crud.py
+++ project/models/crud.py
@@ -3,31 +3,51 @@ from db import get_db_session
 from models.db_models import Book, Author, Genre
 from schemas.simple_shemas import *
 
+def create_dict(**kwargs):
+    return kwargs
+
 
 def get_book_by_id(database: Session, book_id: int):
     return database.query(Book).get(book_id)
 
 
 def create_book(database: Session, schema: BookCreate):
-    book = Book(schema.title,
-                schema.isbn,
-                schema.year_release,
-                schema.price,
-                schema.count,
-                schema.genre_id)
-    
+    author_ids_lst = []
+    book = Book()
+
+    book.title = schema.title
+    book.isbn = schema.isbn            
+    book.year_release = schema.year_release            
+    book.count = schema.count 
+    book.price = schema.price           
+           
+
     genre = database.query(Genre).get(schema.genre_id)
     if not genre:
         return None
+
+    book.genre_id = schema.genre_id  
 
     for i in schema.author_ids:
         author = database.query(Author).get(i)
         if not author:
             return None
         book.authors.append(author)
+        author_ids_lst.append(i)
+
+    book_data = create_dict(
+        title=book.title,
+        isbn=book.isbn,
+        year_release=book.year_release,
+        count=book.count,
+        price=book.price,
+        genre_id=book.genre_id,
+        author_ids=author_ids_lst
+    )
+    
     database.add(book)
     database.commit()
-    return book
+    return book_data
 
 
 def delete_book_by_id(database: Session, book_id: int):
